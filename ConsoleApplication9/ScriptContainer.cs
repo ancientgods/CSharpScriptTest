c4a1b70a36e4ba6670550a020eb35174698819c0
Ancientgods <devoogdlaurens@hotmail.com>
2015-09-14 22:13:09 +0200

    Yep, this was missing

---
+++ ConsoleApplication9/ScriptContainer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using CSScriptLibrary;
+
+namespace Test
+{
+    public class ScriptContainer : IDisposable
+    {
+        public static Dictionary<string, Script> Scripts = new Dictionary<string, Script>();
+        public Script Script { get { return Scripts.ContainsKey(_name) ? Scripts[_name] : null; } }
+        private string _name;
+        public string Name { get { return _name; } }
+        private AppDomain _appDomain;
+
+
+        public ScriptContainer(AppDomain domain, string name)
+        {
+            _name = name;
+            _appDomain = domain;
+
+            _appDomain.Execute<string>(Initialize, name);
+        }
+
+        public static void Initialize(string scriptName)
+        {
+            try
+            {
+
+                Script script = CSScript.Evaluator.LoadFile<Script>(scriptName);
+                script.Initialize();
+
+                Scripts.Add(scriptName, script);
+                foreach (KeyValuePair<string, Script> kvp in Scripts)
+                    Console.WriteLine(kvp.Key);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Script format!");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine(Scripts.Count);
+
+            foreach (KeyValuePair<string, Script> kvp in Scripts)
+                Console.WriteLine(kvp.Key);
+
+            Console.WriteLine("test");
+            if (Script != null)
+            {
+                Console.WriteLine("test 2");
+                Script.Dispose();
+                Scripts.Remove(Name);
+            }
+            _appDomain.Unload();
+        }
+    }
+}
